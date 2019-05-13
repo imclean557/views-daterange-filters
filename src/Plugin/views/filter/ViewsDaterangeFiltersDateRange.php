d0c7d9a816e5eeb78498f24fe95a6416db603863
Ian McLean <imclean@digitalink.com.au>
2019-05-13 14:25:17 +1000

    Add Overlaps operation, coding standards fixes.

--- src/Plugin/views/filter/ViewsDaterangeFiltersDateRange.php
+++ src/Plugin/views/filter/ViewsDaterangeFiltersDateRange.php
@@ -18,20 +18,38 @@ use Drupal\datetime\Plugin\views\filter\Date;
  * @ViewsFilter("views_daterange_filters_daterange")
  */
 class ViewsDaterangeFiltersDateRange extends Date implements ContainerFactoryPluginInterface {
-  
+
+  /**
+   * {@inheritdoc}
+   *
+   * @return array
+   *   Array of operators.
+   */
   public function operators() {
     $operators = parent::operators();
     $operators['includes'] = [
-      'title' => $this->t('Range includes'),
+      'title' => $this->t('Includes'),
       'method' => 'opIncludes',
       'short' => $this->t('includes'),
       'values' => 1,
     ];
-    
+    $operators['overlaps'] = [
+      'title' => $this->t('Overlaps'),
+      'method' => 'opOverlaps',
+      'short' => $this->t('within'),
+      'values' => 2,
+    ];
+
     return $operators;
   }
-  
-  protected function opIncludes($field) {    
+
+  /**
+   * Filters by operator includes.
+   *
+   * @param mixed $field
+   *   The field.
+   */
+  protected function opIncludes($field) {
     $end_field = substr($field, 0, -6) . '_end_value';
 
     $timezone = $this->getTimezone();
@@ -40,12 +58,41 @@ class ViewsDaterangeFiltersDateRange extends Date implements ContainerFactoryPlu
     // Convert to ISO. UTC timezone is used since dates are stored in UTC.
     $value = new DateTimePlus($this->value['value'], new \DateTimeZone($timezone));
     $value = $this->query->getDateFormat($this->query->getDateField("'" . $this->dateFormatter->format($value->getTimestamp() + $origin_offset, 'custom', DateTimeItemInterface::DATETIME_STORAGE_FORMAT, DateTimeItemInterface::STORAGE_TIMEZONE) . "'", TRUE, $this->calculateOffset), $this->dateFormat, TRUE);
-    
+
     $field = $this->query->getDateFormat($this->query->getDateField($field, TRUE, $this->calculateOffset), $this->dateFormat, TRUE);
     $end_field = $this->query->getDateFormat($this->query->getDateField($end_field, TRUE, $this->calculateOffset), $this->dateFormat, TRUE);
-    
-    // This is safe because we are manually scrubbing the value.   
+
     $this->query->addWhereExpression($this->options['group'], "$value BETWEEN $field AND $end_field");
+  }
+
+  /**
+   * Filters by operator within range.
+   *
+   * @param object $field
+   *   The views field.
+   */
+  protected function opOverlaps($field) {
+    $end_field = substr($field, 0, -6) . '_end_value';
+
+    $timezone = $this->getTimezone();
+    $origin_offset = $this->getOffset($this->value['min'], $timezone);
+
+    // Although both 'min' and 'max' values are required, default empty 'min'
+    // value as UNIX timestamp 0.
+    $min = (!empty($this->value['min'])) ? $this->value['min'] : '@0';
+
+    // Convert to ISO format and format for query. UTC timezone is used since
+    // dates are stored in UTC.
+    $a = new DateTimePlus($min, new \DateTimeZone($timezone));
+    $a = $this->query->getDateFormat($this->query->getDateField("'" . $this->dateFormatter->format($a->getTimestamp() + $origin_offset, 'custom', DateTimeItemInterface::DATETIME_STORAGE_FORMAT, DateTimeItemInterface::STORAGE_TIMEZONE) . "'", TRUE, $this->calculateOffset), $this->dateFormat, TRUE);
+    $b = new DateTimePlus($this->value['max'], new \DateTimeZone($timezone));
+    $b = $this->query->getDateFormat($this->query->getDateField("'" . $this->dateFormatter->format($b->getTimestamp() + $origin_offset, 'custom', DateTimeItemInterface::DATETIME_STORAGE_FORMAT, DateTimeItemInterface::STORAGE_TIMEZONE) . "'", TRUE, $this->calculateOffset), $this->dateFormat, TRUE);
+
+    // This is safe because we are manually scrubbing the values.
+    $operator = strtoupper($this->operator);
+    $field = $this->query->getDateFormat($this->query->getDateField($field, TRUE, $this->calculateOffset), $this->dateFormat, TRUE);
+    $end_field = $this->query->getDateFormat($this->query->getDateField($end_field, TRUE, $this->calculateOffset), $this->dateFormat, TRUE);
+    $this->query->addWhereExpression($this->options['group'], "$a <= $end_field AND $b >= $field");
   }
 
 }
